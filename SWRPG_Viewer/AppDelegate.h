d0ea7d696ca90944a03f03e2997ab8c87a3cccc5
matthew ingwersen <matthewingwersen@matthews-macbook.local>
2017-03-03 20:40:59 -0500

    Initial Commit

---
+++ SWRPG_Viewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWRPG_Viewer
+//
+//  Created by matthew ingwersen on 3/3/17.
+//  Copyright © 2017 matthew ingwersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SWRPG_Viewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SWRPG_Viewer
+//
+//  Created by matthew ingwersen on 3/3/17.
+//  Copyright © 2017 matthew ingwersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
